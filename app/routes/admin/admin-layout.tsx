c6822c98e0bf7907690e7a514a4f67970cf06720
deya19 <deyasaleh2@gmail.com>
2025-05-05 16:19:56 +0300

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Outlet} from "react-router";
+
+export default function AdminLayout() {
+    return (
+        <div className="admin-layout">
+            MobileSidebar
+            <aside className="w-full max-w-[270px] hidden lg:block">
+               Sidebar
+            </aside>
+            <aside className="children">
+              <Outlet/>
+            </aside>
+        </div>
+    )
+}
